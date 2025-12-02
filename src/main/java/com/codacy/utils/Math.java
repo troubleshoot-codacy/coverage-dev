5b7772638e31b99d3e24a6b1c99bfb66c9804ced
Joao Machado <13315199+machadoit@users.noreply.github.com>
2025-12-02 18:06:27 +0100

    Remove duplicate print statements in Math.java

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -23,6 +23,8 @@ public class Math {
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
             System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
             return y - x;
         } else {
             System.out.println("I am covered!");
@@ -31,6 +33,7 @@ public class Math {
             System.out.println("I am covered!");
             System.out.println("I am covered!");
             System.out.println("I am covered!");
+            System.out.println("I am covered!!");
             System.out.println("I am covered!!");
             return x + y;
         }
