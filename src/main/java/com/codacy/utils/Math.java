0fc1d308c855b58e907d41e9bd809f03545f30ac
Joao Machado <13315199+machadoit@users.noreply.github.com>
2025-12-02 19:15:22 +0100

    Add additional print statement for coverage check

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -21,6 +21,7 @@ public class Math {
             System.out.println("Am I am covered??");
             System.out.println("Am I am covered?!");
             System.out.println("Am I am covered???!?");
+            System.out.println("PLEASE!");
             return y - x;
         } else {
             System.out.println("I am covered!");
