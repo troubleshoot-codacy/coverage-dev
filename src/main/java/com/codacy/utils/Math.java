1dcc5f12e6808f91278866efe26abd6026ae28c7
Pedro Pereira <pedro.pereira@codacy.com>
2025-12-12 12:28:58 +0000

    adds complexity 14

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -45,7 +45,6 @@ public class Math {
             System.out.println("I am covered!");
             System.out.println("I am covered!");
             System.out.println("I am covered!");
-            System.out.println("I am covered!");
             if (x == magicNumber) {
                 if (y == magicNumber) {
                     if (whatever == true){
